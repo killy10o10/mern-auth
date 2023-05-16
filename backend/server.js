d8062768312a9e4b05220fe350f4092cc6ce43b6
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-05-16 22:58:19 +0000

    Add env config

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const port = 5000;
+import  express  from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
+const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.get('/', (req, res) => res.send('API running'));
 
-app.listen(port, () => console.log(`Server started on port ${port}. Enter http://127.0.0.1:5000 in your browser`));
+app.listen(port, () => console.log(`Server started on port ${port}. Enter http://127.0.0.1:${port} in your browser`));
