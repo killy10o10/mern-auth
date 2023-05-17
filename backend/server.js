a651ea899b47fcee8c70cf35bca8eb86f47e8d5d
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-05-17 18:36:59 +0000

    Fix error with unknown modules

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import  express  from "express";
 import dotenv from 'dotenv';
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
-
 const app = express();
+app.listen(port, () => console.log(`Server started on port ${port}. Enter http://127.0.0.1:${port} in your browser`));
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => res.send('API running'));
-
-app.listen(port, () => console.log(`Server started on port ${port}. Enter http://127.0.0.1:${port} in your browser`));
+  
